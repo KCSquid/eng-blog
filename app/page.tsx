58aed823d7218023bd48a2d3ce8a782df15f7eb0
kcsquid <jahvonc9@gmail.com>
2025-05-13 08:59:34 -0400

    fix eslint in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 'use client'
 
-import Image from "next/image";
-import { ArrowRight } from "lucide-react";
-import { SetStateAction, useEffect, useState, ReactNode } from "react";
-
 export default function Home() {
   window.location.href = "/home";
   return null;
